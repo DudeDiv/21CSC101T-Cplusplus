348c6ef66f4076eea9214728c1956a78c4ca68bb
Divyanshu Kumar <113873506+DudeDiv@users.noreply.github.com>
2023-02-01 08:45:04 +0530

    Create userinputbasic.cpp

---
+++ userinputbasic.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c;
+    cout << "Enter value of A:\n";
+    cin >> a;
+    cout << "Enter value of B:\n";
+    cin >> b;
+    c=a+b;
+    cout << c << "\n";
+    cout << a*b << "\n";
+    cout << (a+b)*5 << "\n";
+    return 0;
+}
