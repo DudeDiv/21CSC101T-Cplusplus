958272159157b6deadad7227fe3d01d8dbb78842
Divyanshu Kumar <113873506+DudeDiv@users.noreply.github.com>
2023-02-01 08:56:27 +0530

    Create datatypes.cpp

---
+++ datatypes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int myNum = 5;
+    float chart = 7.8;
+    double icecream = 8.7;
+    char uni = 'b';
+    bool Alok = false;
+    string myTxt = "Broda";
+    cout << myNum << "\n";
+    cout << chart << "\n";
+    cout << icecream << "\n";
+    cout << uni << "\n";
+    cout << Alok << "\n";
+    return 0;
+}
