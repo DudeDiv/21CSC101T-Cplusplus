aa271ddf1a49ef884037899db9f32c972791d288
Divyanshu Kumar <113873506+DudeDiv@users.noreply.github.com>
2023-02-01 09:16:16 +0530

    Create 4datatypes-operations.cpp

---
+++ 4datatypes-operations.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <iostream>
+using namespace std;
+int main() {
+    int myNum = 5;
+    int z = myNum + 5;
+    cout << z << "\n";
+    float bruh, tut;
+    cout << "Enter the value of bruh:\n";
+    cin >> bruh;
+    cout << "Enter the value of tut:\n";
+    cin >> tut;
+    cout << bruh + tut << "\n";
+    string string1 = "Shubbu is ";
+    string string2 = "scared";
+    string fullname = string1 + string2;
+    cout << fullname << "\n";
+    char Alok = 'c';
+    cout << "ASCII (IQ) value of Alok is:" << (int(Alok));
+    return 0;
+}
